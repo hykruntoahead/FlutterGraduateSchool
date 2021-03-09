c42eb9d6118a44e167059f8b1d51edd7a3331bdd
hykruntoahead <1412805450@qq.com>
2021-03-09 20:37:13 +0800

    修复修改文件名后 main.dart引用异常

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app_demo/text_wdget_instances.dart';
+import 'package:flutter_app_demo/text_widget_instances.dart';
 
 void main() {
   runApp(MyApp());
